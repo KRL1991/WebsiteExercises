d574f0d1420e6926aff2f2fe44de9c2a69a3be7c
Kim <70881764+KRL1991@users.noreply.github.com>
2021-10-24 01:30:34 +0200

    Add files via upload

---
+++ Pancake maker/Script.js
@@ -0,0 +1,32 @@
+
+function calculateAmount() {
+
+
+
+let inputPortions = document.getElementById("Portions").value;
+//alert(inputPortions);
+
+
+
+var flour = document.getElementById("Flour").innerHTML = 150 * inputPortions;
+var eggs = document.getElementById("Eggs").innerHTML = 3 * inputPortions;
+var milk = document.getElementById("Milk").innerHTML = 400 * inputPortions;
+var butter = document.getElementById("Butter").innerHTML = 50 * inputPortions;
+var salt = document.getElementById("Salt").innerHTML = 4 *inputPortions;
+
+
+
+
+
+const minFlour = document.getElementById("Flour");
+
+const minEggs = 3;
+const minMilk = 400;
+const minButter = 50;
+const minSalt = 4;
+
+
+
+
+}
+
